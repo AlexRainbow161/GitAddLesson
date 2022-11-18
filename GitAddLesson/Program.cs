68265323f60024e3b6968705903846c8748c91dc
bogovid <bogovid@gloria-jeans.ru>
2022-11-18 20:54:06 +0300

    Добавьте файлы проекта.

---
+++ GitAddLesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitAddLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
